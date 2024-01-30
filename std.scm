bee89b18a387201ca7d24abf53f8527a77ec3a39
Chuigda <icey@icey.tech>
2024-01-30 02:24:57 +0000

    add these things to std

--- std.scm
+++ std.scm
@@ -1,3 +1,22 @@
+(define (<= x y) (or (< x y) (= x y)))
+(define (> x y) (< y x))
+(define (>= x y) (or (> x y) (= x y)))
+(define (!= x y) (not (= x y)))
+
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+(define (caaar x) (car (car (car x))))
+(define (caadr x) (car (car (cdr x))))
+(define (cadar x) (car (cdr (car x))))
+(define (caddr x) (car (cdr (cdr x))))
+(define (cdaar x) (cdr (car (car x))))
+(define (cdadr x) (cdr (car (cdr x))))
+(define (cddar x) (cdr (cdr (car x))))
+(define (cdddr x) (cdr (cdr (cdr x))))
+(define (cadddr x) (car (cdr (cdr (cdr x)))))
+
 (define (length xs)
   (define ret 0)
   (loop
